385601ddf2b03077cf93b9dad8a2fbb73db46281
Conor Campbell <conorcampbell@live.com>
2020-04-30 16:38:09 +0100

    Most of the way through registration creation

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
-from accounts.forms import UserLoginForm
+from django.contrib.auth.decorators import login_required
+from accounts.forms import UserLoginForm, UserRegistrationForm
 
 
 def index(request):
     """Return the index.html file"""
     return render(request, 'index.html')
 
+@login_required
 def logout(request):
     """Log the user out"""
     auth.logout(request)
@@ -15,6 +17,8 @@ def logout(request):
 
 def login(request):
     """Login the user/return a login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
@@ -24,9 +28,15 @@ def login(request):
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect")
             
     else:
         login_form = UserLoginForm()
     return render(request, "login.html", {"login_form": login_form})
+
+def registration(request):
+    """Render the registration page"""
+    registration_form = UserRegistrationForm()
+    return render(request, 'registration.html', {"registration_form": registration_form})
--- django_practice/settings.py
+++ django_practice/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_practice.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'django_practice/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
